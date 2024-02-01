666ded5b284e5092019e788bfff7662a87d7afae
fanpengxin <pengxin.fan@gmail.com>
2024-02-01 15:05:51 +0000

    initial project

---
+++ HelperLibrary/Calculations/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensium.Helpers.Calculations
+{
+    public class Calculator
+    {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Substract(double x, double y) 
+        { 
+            return x - y; 
+        }
+    }
+}
